1a42b96eeadec97d418d2e127643ec716c667621
refatkhan <refatkhan897@gmail.com>
2024-10-29 21:47:34 +0600

    array print with loop

---
+++ class-4/script.js
@@ -0,0 +1,10 @@
+let arr = [5,6,9,7,11,8];
+//single print
+//console.log(arr[0]);
+//console.log(arr[1]);
+
+//print in a loop
+
+    for (let i = 0; i < arr.length; i++) {
+        console.log("index no: "+ i +" the value is: " +arr[i]);
+    }
